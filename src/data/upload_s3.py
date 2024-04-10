81856f07d7a7506767e40b485cb5e541e08f129e
SloughJE <SloughJE@gmail.com>
2024-04-10 16:33:01 -0400

    updates

--- src/data/upload_s3.py
+++ src/data/upload_s3.py
@@ -20,6 +20,18 @@ session = boto3.Session(
 # Create an S3 client using the session
 s3_client = session.client('s3')
 
+def upload_files_in_folder_to_s3(local_folder, bucket_name, object_key_prefix):
+
+    # get list of files in folder
+    file_list = os.listdir(local_folder)
+    for fl in file_list:
+        local_file_path = f"{local_folder}{fl}"
+        object_key = f"{object_key_prefix}{fl}"
+        upload_file_to_s3(local_file_path, bucket_name, object_key)
+    
+    print(f"uploaded all files in {local_folder}")
+
+
 def upload_file_to_s3(local_file_path, bucket_name, object_key):
     try:
         with open(local_file_path, "rb") as f:
