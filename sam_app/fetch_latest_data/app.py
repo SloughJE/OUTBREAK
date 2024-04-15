98a2abbdf8b684b892f4c68cf2aa4b05d4fa9b9a
SloughJE <SloughJE@gmail.com>
2024-04-15 13:02:11 -0400

    updating lambda

--- sam_app/fetch_latest_data/app.py
+++ sam_app/fetch_latest_data/app.py
@@ -5,7 +5,7 @@ from botocore.exceptions import NoCredentialsError, ClientError
 from io import BytesIO
 import pandas as pd
 
-from data_processing import align_data_schema
+from data_processing import align_data_schema, process_dataframe_deepar
 
 def lambda_handler(event, context):
 
@@ -21,6 +21,38 @@ def lambda_handler(event, context):
         except ClientError as e:
             raise e
         return json.loads(get_secret_value_response['SecretString'])
+    
+
+    def get_latest_file_from_s3(bucket, folder):
+        s3 = boto3.client('s3')
+        paginator = s3.get_paginator('list_objects_v2')
+        page_iterator = paginator.paginate(Bucket=bucket, Prefix=folder)
+        latest_file = None
+        latest_date = None
+        for page in page_iterator:
+            if "Contents" in page:
+                for obj in page['Contents']:
+                    file_key = obj['Key']
+                    # Check if the object is a file (not a folder)
+                    if file_key.endswith('.parquet'):
+                        file_date = obj['LastModified']
+                        if not latest_date or file_date > latest_date:
+                            latest_date = file_date
+                            latest_file = file_key
+
+        return latest_file
+
+
+
+    def get_latest_data_from_parquet(bucket, file_key):
+        s3_resource = boto3.resource('s3')
+        obj = s3_resource.Object(bucket_name=bucket, key=file_key)
+        buffer = BytesIO(obj.get()["Body"].read())
+        df = pd.read_parquet(buffer)
+        latest_year = df['year'].max()
+        latest_week = df[df['year'] == latest_year]['week'].max()
+        return latest_year, latest_week
+
 
     # Retrieve the API token
     secret = get_secret()
@@ -31,52 +63,90 @@ def lambda_handler(event, context):
     base_url = "https://data.cdc.gov/resource/x9gk-5huc.json"
     columns = 'states,year,week,label,m1'
     
-    query_url = f"{base_url}?$$app_token={nndss_app_token}&$select={columns}&$where=location1 IS NOT NULL&$order=year DESC, week DESC&$limit=1"
-    response = requests.get(query_url)
-    
-    if response.status_code == 200:
-        latest_record = response.json()
-        if latest_record:
-            latest_year = latest_record[0]['year']
-            latest_week = latest_record[0]['week']
-            
+    # Get the latest data file from S3
+    latest_file_key = get_latest_file_from_s3(bucket_name, bucket_folder)
+    print(f"latest_file_key: {latest_file_key}")
 
-            week_data_query_url = f"{base_url}?$$app_token={nndss_app_token}&$select={columns}&$where=year='{latest_year}' AND week='{latest_week}' AND location1 IS NOT NULL&$limit={limit}"            
-            week_data_response = requests.get(week_data_query_url)
-            
-            if week_data_response.status_code == 200:
-                latest_week_data = week_data_response.json()
+    if latest_file_key:
+        latest_year, latest_week = get_latest_data_from_parquet(bucket_name, latest_file_key)
+        print(f"latest week and year in s3: week {latest_week}, {latest_year}")
+        week_data_response = None
 
-                # Convert JSON to DataFrame
-                df = pd.DataFrame(latest_week_data)
-                df = align_data_schema(df)
-                file_date = df.date.max().strftime('%Y-%m-%d')
-                # Convert DataFrame to Parquet and upload to S3
-                try:
-                    s3_resource = boto3.resource('s3')
-                    file_key = f"{bucket_folder}/weekly_actuals_{file_date}.parquet"
-                    
-                    # Use a buffer for the Parquet file
-                    buffer = BytesIO()
-                    df.to_parquet(buffer, index=False)
-                    
-                    # Reset buffer position to the start
-                    buffer.seek(0)
-                    s3_resource.Object(bucket_name, file_key).put(Body=buffer.getvalue())
-                    print(f"Parquet file for the most recent week of year {latest_year}, week {latest_week} saved to S3")
-                except NoCredentialsError:
-                    print("Credentials not available")
+        # Get the latest data date from the API
+        query_url = f"{base_url}?$$app_token={nndss_app_token}&$select=year,week&$where=location1 IS NOT NULL&$order=year DESC, week DESC&$limit=1"
+        response = requests.get(query_url)
 
+        if response.status_code == 200:
+            latest_record = response.json()
+            if latest_record:
+                api_latest_year = int(latest_record[0]['year'])
+                api_latest_week = int(latest_record[0]['week'])
+
+                # Only proceed if the API data is newer than the S3 data
+                if (api_latest_year > latest_year) or (api_latest_year == latest_year and api_latest_week > latest_week):
+                    week_data_query_url = f"{base_url}?$$app_token={nndss_app_token}&$select=year,week,states,label,m1&$where=year='{api_latest_year}' AND week='{api_latest_week}' AND location1 IS NOT NULL&$limit={limit}"
+                    week_data_response = requests.get(week_data_query_url)
+                    if week_data_response.status_code == 200:
+                        latest_week_data = week_data_response.json()
+
+                        # Convert JSON to DataFrame
+                        df = pd.DataFrame(latest_week_data)
+                        df = align_data_schema(df)
+                        file_date = df.date.max().strftime('%Y-%m-%d')
+                        # Convert DataFrame to Parquet and upload to S3
+                        try:
+                            s3 = boto3.client('s3')
+                            file_key = f"{bucket_folder}/weekly_actuals_{file_date}.parquet"
+                            
+                            # Use a buffer for the Parquet file
+                            buffer = BytesIO()
+                            df.to_parquet(buffer, index=False)
+                            buffer.seek(0)
+                            s3.put_object(Bucket=bucket_name, Key=file_key, Body=buffer.getvalue())
+                            print(f"Parquet file for the most recent week of year {latest_year}, week {latest_week} saved to S3")
+
+                            # now we create the DeepAR training data with this new data included
+                            print("creating DeepAR input data ")
+                            json_lines_str, mapping_str, label_to_int_str = process_dataframe_deepar(df)
+                            transformed_key = 'deepar_input_data/deepar_dataset.jsonl'
+                            s3.put_object(Bucket=bucket_name, Key=transformed_key, Body=json_lines_str)
+                            print("Data processed and saved to 'deepar_input_data/deepar_dataset.jsonl'")
+                            mapping_key = 'deepar_input_data/time_series_mapping_with_labels.json'
+                            s3.put_object(Bucket=bucket_name, Key=mapping_key, Body=mapping_str)
+                            print(f"Mapping file saved to s3://{bucket_name}/{mapping_key}")
+                            label_to_int_key = 'deepar_input_data/label_to_int_mapping.json'
+                            s3.put_object(Bucket=bucket_name, Key=label_to_int_key, Body=label_to_int_str)
+                            print(f"Label-to-int mapping file saved to s3://{bucket_name}/{label_to_int_key}")
+
+                            return {
+                            'statusCode': 200,
+                            'body': json.dumps(f"New weekly data pulled for week {latest_week} {latest_year} and saved to {file_key}. DeepAR input data created and saved to {bucket_name}")
+                            }
+                        except NoCredentialsError:
+                            return {
+                                'statusCode': 500,
+                                'body': json.dumps("Credentials not available")
+                            }
+                    else:
+                        return {
+                            'statusCode': 500,
+                            'body': json.dumps(f"Failed to fetch data for the latest week: {week_data_response.status_code}")
+                        }
+                else:
+                    return {
+                        'statusCode': 200,
+                        'body': json.dumps("No recent data found.")
+                    }
+            else:
                 return {
                     'statusCode': 200,
-                    'body': json.dumps(f"Parquet file for the most recent week of year {latest_year}, week {latest_week} saved to S3")
+                    'body': json.dumps("No recent data found.")
                 }
-            else:
-                print(f"Failed to fetch data for the latest week: {week_data_response.status_code}")
         else:
-            print("No recent data found.")
-    else:
-        print(f"Failed to fetch the latest data: {response.status_code}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps(f"Failed to fetch the latest data: {response.status_code}")
+            }
 
     return {
         'statusCode': 500,
--- sam_app/fetch_latest_data/data_processing.py
+++ sam_app/fetch_latest_data/data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+import json
 
 def align_data_schema(df):
     """
@@ -9,7 +10,8 @@ def align_data_schema(df):
     df.rename(columns={'states': 'state'}, inplace=True)  # Renaming 'states' to 'state'
     df['item_id'] = df['state'] + '_' + df['label']  # Using the renamed 'state' column
     df['date'] = pd.to_datetime(df['year'].astype(str) + df['week'].astype(str) + '-1', format='%Y%W-%w')
-    df['new_cases'] = pd.to_numeric(df['m1'], errors='coerce')
+    df['new_cases'] = pd.to_numeric(df['m1'], errors='coerce').fillna(0)
+
     df['week'] = df.week.astype(int)
     df['year'] = df.year.astype(int)
     # Ensure the DataFrame has only the expected columns, in the correct order
@@ -18,3 +20,66 @@ def align_data_schema(df):
     df = df[expected_columns]
     
     return df
+
+
+def process_dataframe_deepar(df):
+
+    # A function to convert NaN values to "NaN" string and others to float
+    def convert_target(target_series):
+        return [float(x) if pd.notna(x) else "NaN" for x in target_series]
+
+    def convert_numpy_int64(obj):
+        if isinstance(obj, dict):
+            return {k: convert_numpy_int64(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [convert_numpy_int64(v) for v in obj]
+        elif isinstance(obj, np.int64):
+            return int(obj)  # Convert numpy.int64 to int
+        else:
+            return obj
+        
+    df['date'] = pd.to_datetime(df['date'])
+    df.sort_values(by=['item_id', 'date'], inplace=True)
+
+    # Randomly shuffle the time series
+    random_groups = pd.Series(np.random.permutation(df['item_id'].unique()), index=df['item_id'].unique())
+    df['random_group'] = df['item_id'].map(random_groups)
+
+    df = df.sort_values(by=['random_group', 'date']).drop('random_group', axis=1).reset_index(drop=True)
+
+    # Encode the 'label' as integers
+    unique_labels = df['label'].unique()
+    cardinality = len(unique_labels)
+
+    label_to_int = {label: idx for idx, label in enumerate(unique_labels)}
+    df['label_encoded'] = df['label'].map(label_to_int)
+
+    # Prepare containers for the JSON Lines and mappings
+    json_lines = []
+    time_series_mapping = {}
+
+    for idx, (item_id, group) in enumerate(df.groupby('item_id')):
+        # Get the encoded label; assuming one label per item_id group
+        encoded_label = group['label_encoded'].iloc[0]
+
+        # Create time series data with 'cat' for static features
+        time_series = {
+            "start": str(group['date'].dt.date.iloc[0]),
+            "target": convert_target(group['new_cases']),
+            # Convert numpy int64 to Python int before including it in the 'cat' field
+            "cat": [int(encoded_label)]  # Ensuring it's a native Python int type
+        }
+        json_lines.append(json.dumps(time_series))
+
+        # Map item_id to its index and label encoding in the JSON Lines file
+        time_series_mapping[item_id] = {'index': idx, 'label_encoded': encoded_label}
+
+    # Convert JSON Lines list to a single string for storage or transmission
+    json_lines_str = "\n".join(json_lines)
+
+    # Optionally, save the mappings as JSON for future reference
+    time_series_mapping = convert_numpy_int64(time_series_mapping)
+    time_series_mapping_json = json.dumps(time_series_mapping)
+    label_to_int_json = json.dumps(label_to_int)
+
+    return json_lines_str, time_series_mapping_json, label_to_int_json
